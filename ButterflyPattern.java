5155cfd7ebce727aa3b12f17a480d3e3a0604bd6
Sayantika Laskar <127471376+SayantikaLaskar@users.noreply.github.com>
2023-12-29 19:29:56 +0530

    Add files via upload

---
+++ ButterflyPattern.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        // Upper part of the butterfly
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            int spaces = 2 * (rows - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        // Lower part of the butterfly
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            int spaces = 2 * (rows - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
