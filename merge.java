dcc7af8e6a722e52bff8139c409233c3eb1fe9a3
Sayantika Laskar <127471376+SayantikaLaskar@users.noreply.github.com>
2024-01-05 19:56:04 +0530

    Add files via upload

---
+++ merge.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class merge {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        Solution solution = new Solution();
+        solution.merge(nums1, m, nums2, n);
+
+        System.out.print("Merged Array: ");
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+
