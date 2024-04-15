110aeea320739e2aa43683693a7dbd46e181defe
Sayantika Laskar <127471376+SayantikaLaskar@users.noreply.github.com>
2024-04-15 21:58:12 +0530

    Add files via upload

---
+++ PalindromeLL.java
@@ -0,0 +1,69 @@
+public class PalindromeLL {
+
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        ListNode hare = head;
+        ListNode turtle = head;
+
+        while (hare.next != null && hare.next.next != null) {
+            hare = hare.next.next;
+            turtle = turtle.next;
+        }
+        return turtle;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode middle = findMiddle(head);
+        ListNode secondHalfStart = reverse(middle.next);
+
+        ListNode firstHalfStart = head;
+        while (secondHalfStart != null) {
+            if (firstHalfStart.val != secondHalfStart.val) {
+                return false;
+            }
+            firstHalfStart = firstHalfStart.next;
+            secondHalfStart = secondHalfStart.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(1);
+
+        PalindromeLL solution = new PalindromeLL();
+
+        boolean isPalindrome = solution.isPalindrome(head);
+        System.out.println("Is the linked list a palindrome? " + isPalindrome);
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
